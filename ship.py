f81f00d98381a697e9137720e55b089ea2dc93ec
splinterific <markgpreston@gmail.com>
2014-10-18 21:31:32 +0100

    updated with file new section

--- ship.py
+++ ship.py
@@ -2,42 +2,79 @@ from xml.dom.minidom import Document
 doc = Document()
 
 # this should not be in the closing tag
-reqshipmentval = doc.createElement('req:ShipmentValidateRequestAP xmlns:req="http://www.dhl.com" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.dhl.com ship-val-req_AP.xsd"')
-doc.appendChild(reqshipmentval)
+reqshipmentvalidate = doc.createElement('req:ShipmentValidateRequestAP xmlns:req="http://www.dhl.com" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.dhl.com ship-val-req_AP.xsd"')
+doc.appendChild(reqshipmentvalidate)
 
+
+# <Request>
 request = doc.createElement("Request")
-reqshipmentval.appendChild(request)
-
+reqshipmentvalidate.appendChild(request)
 
 serviceheader = doc.createElement("ServiceHeader")
 request.appendChild(serviceheader)
 
-
 messagetime = doc.createElement("MessageTime")
 serviceheader.appendChild(messagetime)
-text = doc.createTextNode("Example Message time")
+text = doc.createTextNode("time")
 messagetime.appendChild(text)
-
 
 messagereference = doc.createElement("MessageReference")
 serviceheader.appendChild(messagereference)
 text = doc.createTextNode("Example Message Reference")
-# i can use raw_input to caputre the required reference, perhaps password?
-# text = doc.createTextNode(raw_input("What do you want your reference to be? "))
 messagereference.appendChild(text)
-
 
 siteid = doc.createElement("SiteID")
 serviceheader.appendChild(siteid)
 text = doc.createTextNode("User ID")
 siteid.appendChild(text)
 
-
 pword = doc.createElement("Password")
 serviceheader.appendChild(pword)
 text = doc.createTextNode("PA44word")
 pword.appendChild(text)
+# </Request>
 
+
+langcode = doc.createElement("LanguageCode")
+reqshipmentvalidate.appendChild(langcode)
+text = doc.createTextNode("en")
+langcode.appendChild(text)
+
+piecesenab = doc.createElement("PiecesEnabled")
+reqshipmentvalidate.appendChild(piecesenab)
+text = doc.createTextNode("Y")
+piecesenab.appendChild(text)
+
+
+# <Billing>
+billing = doc.createElement("Billing")
+reqshipmentvalidate.appendChild(billing)
+
+shipperaccountnum = doc.createElement("ShipperAccountNumber")
+billing.appendChild(shipperaccountnum)
+text = doc.createTextNode("96000111")
+shipperaccountnum.appendChild(text)
+
+shippingpaymenttype = doc.createElement("ShippingPaymentType")
+billing.appendChild(shippingpaymenttype)
+text = doc.createTextNode("S")
+shippingpaymenttype.appendChild(text)
+
+billingaccountnum = doc.createElement("BillingAccountNumber")
+billing.appendChild(billingaccountnum)
+text = doc.createTextNode("96000111")
+billingaccountnum.appendChild(text)
+
+dutypaymenttype = doc.createElement("DutyPaymentType")
+billing.appendChild(dutypaymenttype)
+text = doc.createTextNode("R")
+dutypaymenttype.appendChild(text)
+
+dutyaccountnum = doc.createElement("DutyAccountNumber")
+billing.appendChild(dutyaccountnum)
+text = doc.createTextNode("96000111")
+dutyaccountnum.appendChild(text)
+# </Billing>
 
 
 filename = "ship.xml"
