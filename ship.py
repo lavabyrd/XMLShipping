621708e2c72b380b219ea43f1af53e222fc40fcd
splinterific <markgpreston@gmail.com>
2014-10-19 20:49:59 +0100

    updated with cnee info and text output file

--- ship.py
+++ ship.py
@@ -77,6 +77,119 @@ dutyaccountnum.appendChild(text)
 # </Billing>
 
 
+# <Consignee>
+consignee = doc.createElement("Consignee")
+reqshipmentvalidate.appendChild(consignee)
+
+cneecompname = doc.createElement("CompanyName")
+consignee.appendChild(cneecompname)
+text = doc.createTextNode("Cnee company Name")
+cneecompname.appendChild(text)
+
+cneeadd1 = doc.createElement("AddressLine")
+consignee.appendChild(cneeadd1)
+text = doc.createTextNode("Cnee Address Line 1")
+cneeadd1.appendChild(text)
+
+cneeadd2 = doc.createElement("AddressLine")
+consignee.appendChild(cneeadd2)
+text = doc.createTextNode("Cnee Address Line 2")
+cneeadd2.appendChild(text)
+
+cneecity = doc.createElement("City")
+consignee.appendChild(cneecity)
+text =doc.createTextNode("Cnee City")
+cneecity.appendChild(text)
+
+cneedivision = doc.createElement("Division")
+consignee.appendChild(cneedivision)
+text =doc.createTextNode("Cnee division")
+cneedivision.appendChild(text)
+
+cneedivcd = doc.createElement("DivisionCode")
+consignee.appendChild(cneedivcd)
+text =doc.createTextNode("Cnee Division Code")
+cneedivcd.appendChild(text)
+
+cneepostcd = doc.createElement("Postalcode")
+consignee.appendChild(cneepostcd)
+text =doc.createTextNode("Cnee Post Code")
+cneepostcd.appendChild(text)
+
+cneecountrycd = doc.createElement("CountryCode")
+consignee.appendChild(cneecountrycd)
+text =doc.createTextNode("Cnee 2 digit country code")
+cneecountrycd.appendChild(text)
+
+cneecountryname = doc.createElement("CountryName")
+consignee.appendChild(cneecountryname)
+text =doc.createTextNode("Cnee country Name Full")
+cneecountryname.appendChild(text)
+
+cneecontactinfo = doc.createElement("Contact")
+consignee.appendChild(cneecontactinfo)
+
+cneecontactname = doc.createElement("PersonName")
+cneecontactinfo.appendChild(cneecontactname)
+text =doc.createTextNode("Cnee Contact Name")
+cneecontactname.appendChild(text)
+
+cneephone = doc.createElement("PhoneNumber")
+cneecontactinfo.appendChild(cneephone)
+text =doc.createTextNode("Cnee Number")
+cneephone.appendChild(text)
+
+cneephoneext = doc.createElement("PhoneExtension")
+cneecontactinfo.appendChild(cneephoneext)
+text =doc.createTextNode("Cnee Phone Extension")
+cneephoneext.appendChild(text)
+
+cneefax = doc.createElement("FaxNumber")
+cneecontactinfo.appendChild(cneefax)
+text =doc.createTextNode("Cnee Fax Number")
+cneefax.appendChild(text)
+
+cneetelex = doc.createElement("Telex")
+cneecontactinfo.appendChild(cneetelex)
+text =doc.createTextNode("Cnee Telex Number")
+cneetelex.appendChild(text)
+
+cneeemailform = doc.createElement("Email")
+cneecontactinfo.appendChild(cneeemailform)
+
+cneeemailfrom = doc.createElement("From")
+cneeemailform.appendChild(cneeemailfrom)
+text = doc.createTextNode("Cnee@Cneecompany.com")
+cneeemailfrom.appendChild(text)
+
+cneeemailto = doc.createElement("To")
+cneeemailform.appendChild(cneeemailto)
+text = doc.createTextNode("To@cneecompany.com")
+cneeemailto.appendChild(text)
+
+cneeemailcc = doc.createElement("cc")
+cneeemailform.appendChild(cneeemailcc)
+text = doc.createTextNode("CC@Cneecompany.com")
+cneeemailcc.appendChild(text)
+
+cneeemailsub = doc.createElement("Subject")
+cneeemailform.appendChild(cneeemailsub)
+text = doc.createTextNode("Cnee Email Subject")
+cneeemailsub.appendChild(text)
+
+cneereplyto = doc.createElement("ReplyTo")
+cneeemailform.appendChild(cneereplyto)
+text = doc.createTextNode("Reply@cneecompany.com")
+cneereplyto.appendChild(text)
+
+cneeemailbody = doc.createElement("Body")
+cneeemailform.appendChild(cneeemailbody)
+text = doc.createTextNode("Cnee Email Body")
+cneeemailbody.appendChild(text)
+# </Consignee>
+
+
+
 filename = "ship.xml"
 f = open(filename, "w")
 f.write(doc.toprettyxml (indent = "	"))
